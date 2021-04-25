17a11911e8b909753aee4e670812da52fc8a546a
atrox3d <atrox3d@gmail.com>
2021-04-25 11:47:27 +0200

    fix command line argument

--- client/postman.py
+++ client/postman.py
@@ -36,14 +36,20 @@ def get_server(server="http://localhost:5000"):
                 Debug.info(f"found {path}")
                 config = json.load(fp)
                 Debug.info("config content:", config)
-                server = config["SERVER"]
+                protocol = "http"
+                host = config["HOST"]
+                port = config["PORT"]
+                server = f"{protocol}://{host}:{port}"
                 Debug.info(f"SERVER from {path}: {server}")
         except Exception as e:
             Debug.info(repr(e))
 
     try:
         Debug.info(f"trying sys.argv[1]...")
-        server = sys.argv[1]
+        protocol = "http"
+        host = sys.argv[1]
+        port = sys.argv[2]
+        server = f"{protocol}://{host}:{port}"
         Debug.info(f"SERVER from sys.argv[1]: {server}")
     except IndexError as ie:
         Debug.info(repr(ie))
